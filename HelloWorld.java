233f39ef05cc8699d8c888bce814a73d0757f82a
Tanvi Gupta <tg7177@srmist.edu.in>
2026-04-06 10:51:11 +0530

    UC3: Display Hello with name or default World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+
+        String name;
+
+        if (args.length > 0 && !args[0].isEmpty()) {
+            name = args[0];
         } else {
-            System.out.println("Hello World");
+            name = "World";
         }
+
+        System.out.println("Hello " + name);
     }
 }
