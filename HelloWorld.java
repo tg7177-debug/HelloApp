7550fa8e7f4957918a38cc7a7c37e40a7e8721ae
Tanvi Gupta <tg7177@srmist.edu.in>
2026-04-06 10:52:44 +0530

    UC5: Use enhanced for-loop to display greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,18 +2,21 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
-            System.out.print("Hello ");
-            
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-                
-                // Add comma between names (except last)
-                if (i < args.length - 1) {
+            System.out.print("Hello, ");
+
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
                     System.out.print(", ");
                 }
+                System.out.print(name);
+                first = false;
             }
+
+            System.out.println("!");
         }
     }
 }
